8524b11ae22fda3270dc56bca5f6faed5afb9a06
Wickerance <therealerwin53@gmail.com>
2025-11-05 20:08:20 +0300

    Assignment 2 - Task 1: Implemented palindrome checker function.

---
+++ Task1/palindrome_checker.py
@@ -0,0 +1,7 @@
+﻿# Palindrome Checker
+def is_palindrome(text):
+    cleaned_text = ''.join(filter(str.isalnum, text)).lower()
+    return cleaned_text == cleaned_text[::-1]
+print(f"'madam': {is_palindrome('madam')}")        
+print(f"'A man, a plan, a canal: Panama': {is_palindrome('A man, a plan, a canal: Panama')}") 
+print(f"'hello': {is_palindrome('hello')}")      
